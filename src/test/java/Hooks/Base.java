659a4f885cbbd06980daa99ed8909065d73dbcf6
Amrita Jha <47865497+amritagithub@users.noreply.github.com>
2024-05-19 00:33:24 +0530

    Rahul shetty ecommerce site

---
+++ src/test/java/Hooks/Base.java
@@ -0,0 +1,42 @@
+package Hooks;
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+
+
+public class Base {
+	
+	WebDriver driver;
+	@Test
+	public void launch()
+	{
+	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		 driver.get("https://rahulshettyacademy.com/angularpractice/shop");
+		List<WebElement> ele=driver.findElements(By.xpath("//button[contains(text(),\"Add\")]"));
+		for(WebElement ele1: ele)
+		{JavascriptExecutor js =(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].style.border='5px dotted yellow'", ele1);
+		
+			ele1.click();
+		}
+		driver.findElement(By.xpath("//*[@class='nav-link btn btn-primary']")).click();
+	}
+
+	@AfterClass
+	public void closet()
+	{
+		//driver.close();
+	}
+}
